8bfcbd8cf89a5efad36cb6d611c5458a4439489c
Appie Verschoor <xiffy@xiffy.nl>
2013-10-06 21:19:13 +0200

    Sectie toevoegen, strong op de titel van het artikel

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
 		<h1>Artikelen van <a href="http://decorrespondent.nl/">de Correspondent</a> gevonden op Twitter</h1>
 		<table>
 			<tr>
-				<th>Opgedoken</th><th>Titel / Artikel</th><th>Auteur</th>
+				<th>Opgedoken</th><th>Titel / Artikel</th><th>Auteur</th><th>Sectie</th>
 			</tr>
 
 <?php
@@ -21,11 +21,14 @@ while($row = mysql_fetch_array($res) )
 	$titel = isset($og['title']) ? $og['title'] : substr($row['clean_url'],26);
 	$auth_res = mysql_query('select * from meta where meta.waarde = "'.$og['article:author'].'"');
 	$author = mysql_fetch_array($auth_res);
+	$section_res = mysql_query('select * from meta where meta.waarde = "'.$og['article:section'].'"');
+	$section = mysql_fetch_array($section_res);
 	?>
 	<tr <?php if($i % 2 == 1) echo 'class="odd"'?>>
 		<td><?php echo substr($row['created_at'],8,2); echo '-'.substr($row['created_at'],5,2).' '.substr($row['created_at'],11,5) ?></td>
-		<td><a href="<?php echo $row['share_url'];?>"><?php echo $titel ;?></a></td>
+		<td><strong><a href="<?php echo $row['share_url'];?>"><?php echo $titel ;?></a></strong></td>
 		<td><a href="./meta_art.php?id=<?php echo $author['ID'];?>" title="alle artikelen van deze auteur"><?php echo $author['waarde'];?></a></td>
+		<td><a href="./meta_art.php?id=<?php echo $section['ID'];?>" title="alle artikelen in deze sectie"><?php echo $section['waarde'];?></a></td>
 	</tr>
 	<?php
 	$i++;
