ed766929b137e8d460ac691dbc3a9dbd5831b5fb
Appie Verschoor <xiffy@xiffy.nl>
2013-10-09 11:02:29 +0200

    Publicatietijd in de tael weergeven, verantwoording en de gevonden-tijd als ABBR in de tabel, dat lijkt me beter.

--- footer.php
+++ footer.php
@@ -1,5 +1,6 @@
 ﻿<footer>
 	<p>Als je de artikelen van <a href="http://decorrespondent.nl/">de Correspondent</a> het lezen waard vindt, <a href="https://decorrespondent.nl/word-lid">neem dan een abonnement</a>. Deze service is bedoeld als een proof of concept, zal waarschijnlijk nooit uitputtend alle artikelen van de correspondent vinden, simpelweg omdat sommige artikelen niet getwitterd zullen worden.</p>
+	<p>De artikelen in de tabel staan chronoligisch omgekeerd in de volgorde waarin de bot ze op twitter heeft gevonden. Het kan dus zijn dat een eerder gepubliceerd artikel boven een vers artikel opduikt in de lijst. Dit betekent dat het artikel later op twitter is verschenen. Wanneer je over de datum gaat met de muis zie je wanneer de bot het artikel op twitter heeft gevonden.</p>
 	<p>de code van dit project vind je op github: <a href="http://github.com/xiffy/dcrrspndnt/">project: dcrrspndnt</a>. Patches en pull requests zijn welkom.</p>
 	<p>buy me a beer: 19nUw7xTwZ5EzTqcJzwg6pavRerUD8Mx7F (bitcoin)</p>
 </footer>
--- index.php
+++ index.php
@@ -30,7 +30,7 @@ $res = mysql_query('select * from artikelen order by created_at desc limit '.$st
 		<div class="center">
 		<table>
 			<tr>
-				<th>Opgedoken</th><th>Titel / Artikel</th><th>Auteur</th><th>Sectie</th>
+				<th>Gepubliceerd</th><th>Titel / Artikel</th><th>Auteur</th><th>Sectie</th>
 			</tr>
 <?php
 while($row = mysql_fetch_array($res) )
@@ -42,10 +42,14 @@ while($row = mysql_fetch_array($res) )
 	$author = mysql_fetch_array($auth_res);
 	$section_res = mysql_query('select * from meta where meta.waarde = "'.$og['article:section'].'"');
 	$section = mysql_fetch_array($section_res);
+	$pubtime_res = mysql_query('select * from meta where meta.waarde = "'.$og['article:published_time'].'"');
+	$pubtime = mysql_fetch_array($pubtime_res);
+	$display_time = isset($pubtime['waarde']) ? strftime('%e %b %H:%M', $pubtime['waarde']) : substr($row['created_at'],8,2).'-'.substr($row['created_at'],5,2).' '.substr($row['created_at'],11,5);
+	$found_at = substr($row['created_at'],8,2).'-'.substr($row['created_at'],5,2).' '.substr($row['created_at'],11,5);
 	?>
 
 			<tr <?php if($i % 2 == 1) echo 'class="odd"'?>>
-				<td><?php echo substr($row['created_at'],8,2); echo '-'.substr($row['created_at'],5,2).' '.substr($row['created_at'],11,5) ?></td>
+				<td><abbr title="gevonden op: <?php echo $found_at;?>"><?php echo $display_time ?></abbr></td>
 				<td><strong><a href="<?php echo $row['share_url'];?>" title="<?php echo $description ?>"><?php echo $titel ;?></a></strong></td>
 				<td><a href="./meta_art.php?id=<?php echo $author['ID'];?>" title="alle artikelen van deze auteur"><?php echo $author['waarde'];?></a></td>
 				<td><a href="./meta_art.php?id=<?php echo $section['ID'];?>" title="alle artikelen in deze sectie"><?php echo $section['waarde'];?></a></td>
@@ -76,11 +80,3 @@ while($row = mysql_fetch_array($res) )
 <?php @include('ga.inc.php') ?>
 
 </html>
-
-<?php
-/*
-alle artikelen per auteur:
-select *, count(meta_artikel.id) from meta join meta_artikel on meta_artikel.meta_id = meta.ID where type = 'article:author' group by meta.ID
-en per sectie:
-select *, count(meta_artikel.id) from meta join meta_artikel on meta_artikel.meta_id = meta.ID where type = 'article:section' group by meta.ID
-*/
