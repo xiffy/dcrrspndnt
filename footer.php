c387871f1dc9545659f0f6cf92cbaddfd438c62e
Appie Verschoor <xiffy@xiffy.nl>
2013-10-09 14:19:09 +0200

    Follow on twitter knopje toegevoegd

--- footer.php
+++ footer.php
@@ -1,4 +1,8 @@
 ﻿<footer>
+	<div class="follow-me">
+		<a href="https://twitter.com/dcrrspndnt" class="twitter-follow-button" data-show-count="false" data-lang="nl">Volg @dcrrspndnt</a>
+		<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src="//platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
+	</div>
 	<p>Als je de artikelen van <a href="http://decorrespondent.nl/">de Correspondent</a> het lezen waard vindt, <a href="https://decorrespondent.nl/word-lid">neem dan een abonnement</a>. Deze service is bedoeld als een proof of concept, zal waarschijnlijk nooit uitputtend alle artikelen van de correspondent vinden, simpelweg omdat sommige artikelen niet getwitterd zullen worden.</p>
 	<p>De artikelen in de tabel staan chronologisch omgekeerd in de volgorde waarin de bot ze op twitter heeft gevonden. Het kan dus zijn dat een eerder gepubliceerd artikel boven een vers artikel opduikt in de lijst. Dit betekent dat het artikel later op twitter is verschenen. Wanneer je over de datum gaat met de muis zie je wanneer de bot het artikel op twitter heeft gevonden.</p>
 	<p>de code van dit project vind je op github: <a href="http://github.com/xiffy/dcrrspndnt/">project: dcrrspndnt</a>. Patches en pull requests zijn welkom.</p>
