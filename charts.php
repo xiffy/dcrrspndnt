7b2c33e5d1a7d8fcd33bbce172208100af303b45
Appie Verschoor <xiffy@xiffy.nl>
2013-10-29 23:43:26 +0100

    Tweets van vandaag projecteren zoals op nrctweets

--- charts.php
+++ charts.php
@@ -21,12 +21,22 @@
 require_once('settings.local.php');
 require_once('functions.php');
 include('db.php');
-$tot_tweets_res = mysql_query('select count(tweets.id) as tweet_count, day(tweets.created_at) as  dag, month(tweets.created_at) as maand from tweets where created_at > "2013-10-13 21:00" group by maand, dag');
+
+// Grafiek 1; tweets per dag
+
+$tot_tweets_res = mysql_query('select count(tweets.id) as tweet_count, day(tweets.created_at) as  dag, month(tweets.created_at) as maand from tweets where created_at > "2013-10-13 21:00" group by maand, dag order by day(tweets.created_at) desc limit 0,30');
 
 $label = array();
 $tweets = array();
 $high = 0;
+$rows = array();
 while ($row = mysql_fetch_array($tot_tweets_res))
+{
+	$rows[] = $row;
+}
+$rows = array_reverse($rows);
+
+foreach($rows as $row)
 {
 	$label[] = $row['dag'];
 	$tweets[] = $row['tweet_count'];
@@ -47,9 +57,14 @@ foreach($tweets as $tweet_data)
 }
 $bar_tweet_data = substr($bar_tweet_data, 0, strlen($bar_tweet_data) - 1);
 
+
+
+// Grafiek 2;
+// Tweets per uur en de dagtrend daar op afgezet
 $dagen_res = mysql_query("select day(tweets.created_at) as dagen from tweets group by dagen");
 $dagen = mysql_num_rows($dagen_res);
-$graph_res = mysql_query("select count(tweets.id) as tweet_count, hour(tweets.created_at) as per_uur from tweets where created_at > '2013-10-13 20:59' group by per_uur ");
+
+$graph_res = mysql_query("select count(tweets.id) as tweet_count, hour(tweets.created_at) as per_uur from tweets where created_at > \"2013-10-13 21:00\" group by per_uur ");
 
 $high = 0;
 $hour_label = '';
@@ -59,11 +74,37 @@ while ($row = mysql_fetch_array($graph_res))
 	$hour_label .= $row['per_uur'].',';
 	$tot = ceil($row['tweet_count'] / $dagen);
 	$hour_tweet_data .= $tot.',';
-	$high = max($high, $tot + 30);
+	$high = max($high, $tot + 20);
 }
-$scaleWidth2 = ceil($high / 10);
+
 $hour_label = substr($hour_label, 0, strlen($hour_label) - 1);
 $hour_tweet_data = substr($hour_tweet_data, 0, strlen($hour_tweet_data) - 1);
+
+// A la Chartbeat, de lijn wordt langer tijdens de dag
+// verschijnt in de uur-trend-grafiek
+// Eerst de tweets van vandaag
+$res_today = mysql_query("select count(tweets.ID) per_hour, hour(tweets.created_at) as the_hour, tweets.created_at from tweets
+where year(tweets.created_at) = year(now() )
+  and month(tweets.created_at) = month(now())
+  and day(tweets.created_at) = day(now() )
+group by the_hour
+order by created_at");
+// verwerken in grafiek-data
+$i=0;
+while ($row = mysql_fetch_array($res_today))
+{
+	while ($i < (int)$row['the_hour'])
+	{
+		$hour_today_data .= '0,';
+		$i++;
+	}
+	$high = max($high, $row['per_hour'] + 20);
+	$hour_today_data .= $row['per_hour'].',';
+	$i++;
+}
+$hour_today_data = substr($hour_today_data, 0, strlen($hour_today_data) - 1);
+$scaleWidth2 = ceil($high / 10);
+
 
 ?>
 
@@ -97,7 +138,7 @@ $hour_tweet_data = substr($hour_tweet_data, 0, strlen($hour_tweet_data) - 1);
 			<h2>Tweets per uur</h2>
 			<canvas id="hour_tweets" height="450" width="800"></canvas>
 			<script>
-				var barOptions = {
+				var lineOptions = {
 					scaleOverride : 1,
 					scaleSteps : 10,
 					//Number - The value jump in the hard coded scale
@@ -106,16 +147,27 @@ $hour_tweet_data = substr($hour_tweet_data, 0, strlen($hour_tweet_data) - 1);
 					scaleStartValue : 0
 
 				}
-				var barChartData = {
+				var lineChartData = {
 					labels: [ <?php echo $hour_label;  ?>],
 					datasets : [ {
 												fillColor   : "rgba(77,83,97,0.5)",
 												strokeColor : "rgba(77,83,97,1)",
+												pointColor : "rgba(77,83,97,1)",
+												pointStrokeColor : "#fff",
 												data : [<?php echo $hour_tweet_data;?>]
-										 } ]
+										 },
+										   {
+										   	fillColor	  : "rgba(192,8,14,0.5)",
+										   	strokeColor : "rgba(192,8,14,1)",
+										   	pointColor : "rgba(192,8,14,1)",
+										   	pointStrokeColor : "#000",
+										   	data: [<?php echo $hour_today_data;?>]
+										  }
+										 ]
 				}
-				var tweetHour = new Chart(document.getElementById("hour_tweets").getContext("2d")).Bar(barChartData, barOptions);
+				var tweetHour = new Chart(document.getElementById("hour_tweets").getContext("2d")).Line(lineChartData, lineOptions);
 			</script>
+			<p>Grijs is de overall trend, rood geeft de tweets van vandaag weer</p>
 		</div>
 		</div>
 <?php include('footer.php') ?>
